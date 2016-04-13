888ee48bbc0c54246e813f6808a8b7b3acffd864
Nick-Nordale <nicknordale@gmail.com>
2016-04-12 22:45:47 -0700

    Done PA1

---
+++ PA1/index.php
@@ -0,0 +1,98 @@
+<?php
+
+$conn = new PDO('mysql:host=nnpa1.cdnehwffc0c2.us-west-2.rds.amazonaws.com:3306;dbname=NBAPLAYERS', 'info344user', 'nbapassword');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if(isset($_GET['playerName'])) {
+	$name = $_GET['playerName'];
+} else {
+	$name = "";
+}
+
+$cols = $conn->query('SELECT * FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`=\'NBAPLAYERS\' AND `TABLE_NAME`=\'PLAYERS\'');
+
+$search = $conn->prepare('SELECT * FROM `PLAYERS` WHERE `Player` LIKE :searchName');
+$search->execute(array(":searchName" => "%" . $name . "%"));
+
+$results = array();
+if ($search->execute()) {
+	while ($row = $search->fetch(PDO::FETCH_ASSOC)) {
+		$results[] = $row;
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>NBA Stats</title>
+
+	<link rel="stylesheet" href="main.css">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+
+	<div class="row" id="search-row">
+		<a href="http://ec2-52-38-169-3.us-west-2.compute.amazonaws.com/">
+			<img id="logo" src="http://www.officialpsds.com/images/thumbs/NBA-logo-psd15019.png" alt="">
+		</a>
+		<script>
+			$( document ).ready(function() {
+				$("#playerName").keyup(function () {
+					var keyword = $("#playerName").val();
+					$.ajax({
+						url: '/index.php',
+						type: 'GET',
+						data: { playerName: keyword },
+						success: function (result) {
+							console.log(keyword);
+							var trim1 = $.trim(result.substr(result.indexOf("<tr id=\"res\">")));
+							var trim2 = $.trim(trim1.substr(0, trim1.indexOf("</tbody>")));
+							$("#table-body").html(trim2);
+						}
+					});
+				});
+			});
+		</script>
+		<form action = "" method = "GET">
+			<label for="playerName">Search for players by name:</label>
+			<fieldset class="form-inline">
+				<input type="text" class="form-control" id="playerName" name="playerName" placeholder="player name">
+				<button type="submit" class="btn btn-primary">Go</button>
+			</fieldset>
+		</form>
+	</div>
+
+	<div class="row" id="results-row">
+		<table class="table table-hover">
+			<thead>
+			<tr>
+				<?php foreach($cols as $attr): ?>
+					<th><?php echo $attr['COLUMN_NAME']; ?></th>
+				<?php endforeach; ?>
+			</tr>
+			</thead>
+			<tbody id="table-body">
+			<?php foreach($results as $player): ?>
+				<tr id="res">
+					<?php foreach($player as $stat): ?>
+						<td><?php echo $stat; ?></td>
+					<?php endforeach; ?>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+</body>
+</html>
---
+++ PA1/readme.txt
@@ -0,0 +1,4 @@
+URL to your AWS instance hosting this website (readme.txt)
+	http://ec2-52-38-169-3.us-west-2.compute.amazonaws.com/
+URL to your GitHub repro with PHP source code (share your GitHub with me & TA)
+	https://github.com/NickNordale/344
